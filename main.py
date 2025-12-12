6821eb09af8e11b11a60fb3b200af3daa021f817
Ronald / <belloconcepcionr@gmail.com>
2025-12-12 16:10:08 -0500

    API de puerto agregada

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from libs.system_info import get_system_info
-from libs.geo_ip import geoIp
+from libs.port_utils import runPort
 
 app = FastAPI()
 
@@ -17,7 +17,10 @@ async def systemInfo():
     info = get_system_info()
     return info
 
-@app.get("/api/geoIp")
-async def geoIpInfo(ip: str):
-    data = geoIp(ip)
-    return data
+@app.post("/api/run-port")
+async def openPort(
+    port: str = Form(...),
+    loc: str = Form(...)
+):
+    server = runPort(port, loc)
+    return server
